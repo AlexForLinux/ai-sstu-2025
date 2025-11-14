9008fbe4a4a1b84e50fdec2bdba1e97924a3a8de
Tulitanir <ne4aev.denis2010@yandex.ru>
2025-11-14 09:55:09 +0400

    remove extra population

--- PlantDiseaseDetector/app/src/main/java/com/example/plantdiseasedetector/PlantDiseaseDetector.kt
+++ PlantDiseaseDetector/app/src/main/java/com/example/plantdiseasedetector/PlantDiseaseDetector.kt
@@ -10,21 +10,4 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltAndroidApp
-class PlantDiseaseDetector : Application() {
-
-    @Inject
-    lateinit var diseaseDao: DiseaseDao
-
-    override fun onCreate() {
-        super.onCreate()
-
-        CoroutineScope(Dispatchers.IO).launch {
-            if (diseaseDao.getAllDiseasesOnce().isEmpty()) {
-                diseaseDao.insertDisease(Disease(title = "Мучнистая Роса", shortDesc = "Мучнистая Роса"))
-                diseaseDao.insertDisease(Disease(title = "Ржавчина", shortDesc = "Ржавчина"))
-                diseaseDao.insertDisease(Disease(title = "Пятнистость листьев", shortDesc = "Пятнистость листьев"))
-                diseaseDao.insertDisease(Disease(title = "Поражение слизнями", shortDesc = "Поражение слизнями"))
-            }
-        }
-    }
-}
+class PlantDiseaseDetector : Application()
