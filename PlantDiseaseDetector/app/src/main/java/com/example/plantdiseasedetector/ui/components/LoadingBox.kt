27f99c9cfff9d6dc55bf4f9f0d53e17f980287a8
MrPushistik <a.ovchinnikov.03@mail.ru>
2025-11-20 03:21:27 +0400

    New Cards for Loading and Error

---
+++ PlantDiseaseDetector/app/src/main/java/com/example/plantdiseasedetector/ui/components/LoadingBox.kt
@@ -0,0 +1,68 @@
+package com.example.plantdiseasedetector.ui.components
+
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun LoadingBox(
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colorScheme.primary,
+    strokeWidth: Dp = 4.dp,
+    size: Dp = 48.dp
+) {
+    val infiniteTransition = rememberInfiniteTransition(label = "loading")
+
+    val rotation by infiniteTransition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        ),
+        label = "rotation"
+    )
+
+    Box(
+        modifier = modifier
+            .size(size),
+        contentAlignment = Alignment.Center,
+    ) {
+        Canvas (modifier = Modifier.matchParentSize()) {
+            drawCircle(
+                color = color.copy(alpha = 0.2f),
+                radius = size.toPx() / 2,
+                style = Stroke(width = strokeWidth.toPx())
+            )
+
+            drawArc(
+                color = color,
+                startAngle = rotation - 90,
+                sweepAngle = 90f,
+                useCenter = false,
+                style = Stroke(
+                    width = strokeWidth.toPx(),
+                    cap = StrokeCap.Round
+                ),
+                size = Size(size.toPx(), size.toPx())
+            )
+        }
+    }
+}
