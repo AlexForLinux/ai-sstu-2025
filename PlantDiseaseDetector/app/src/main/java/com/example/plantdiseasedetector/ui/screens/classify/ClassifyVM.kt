2b1feac4581ee8cdc7ecb7952c7e0beb0079e4e3
Alex <a.ovchinnikov.03@mail.ru>
2025-11-23 23:31:32 +0400

    Updated ClassifyVM - Result To History

--- PlantDiseaseDetector/app/src/main/java/com/example/plantdiseasedetector/ui/screens/classify/ClassifyVM.kt
+++ PlantDiseaseDetector/app/src/main/java/com/example/plantdiseasedetector/ui/screens/classify/ClassifyVM.kt
@@ -7,19 +7,25 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.plantdiseasedetector.data.model.DiseasePrecision
+import com.example.plantdiseasedetector.data.model.ReportItem
 import com.example.plantdiseasedetector.data.repository.ClassifyRepository
 import com.example.plantdiseasedetector.data.repository.DiseaseRepository
+import com.example.plantdiseasedetector.data.repository.HistoryRepository
+import com.example.plantdiseasedetector.data.repository.ImageRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.launch
+import java.util.Date
 import javax.inject.Inject
 
 @HiltViewModel
 class ClassifyVM @Inject constructor (
-    private val classifyRepository: ClassifyRepository
+    private val classifyRepository: ClassifyRepository,
+    private val historyRepository: HistoryRepository,
+    private val imageRepository: ImageRepository
 ) : ViewModel() {
 
     var loadedBitmap by mutableStateOf<Bitmap?>(null)
@@ -39,14 +45,29 @@ class ClassifyVM @Inject constructor (
         _predictionsState.value = PredictionDataState.Loading
 
         viewModelScope.launch {
-            classifyRepository.predict(loadedBitmap)
-            .catch { exception ->
-                _predictionsState.value = PredictionDataState.Error(
-                    "Не удалось обработать изображение: ${exception.message}"
-                )
-            }
-            .collect { modelPrediction ->
-                _predictionsState.value = PredictionDataState.Success(modelPrediction)
+            loadedBitmap?.let {
+                classifyRepository.predict(it)
+                    .catch { exception ->
+                        _predictionsState.value = PredictionDataState.Error(
+                            "Не удалось обработать изображение: ${exception.message}"
+                        )
+                    }
+                    .collect { modelPrediction ->
+        //                TODO: should they granted to complete both?
+                        _predictionsState.value = PredictionDataState.Success(modelPrediction)
+
+                        val items = modelPrediction.precisions.map { precision ->
+                            ReportItem(
+                                diseaseId = if (precision.diseaseId != "healthy") precision.diseaseId else null,
+                                precision = precision.precision
+                            )
+                        }
+
+                        val imageFileName = imageRepository.generateFileName()
+                        val imagePath = imageRepository.saveBitmap(loadedBitmap!!, imageFileName)
+                        if (imagePath != null)
+                            historyRepository.createReport(imagePath, items)
+                    }
             }
         }
     }
